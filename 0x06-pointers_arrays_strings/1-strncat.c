7cab3dd81e499b005fa84ffb0b703b8f739cef75
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-09-21 12:45:28 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,19 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
 		;
 
 	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
-		;
+
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
 
