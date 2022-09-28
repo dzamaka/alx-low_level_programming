25a5a736ec183035cfbcb909dbe7a2cffc7ae48d
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-09-28 14:37:04 -0700

    My commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int c;
-
-	c = 0;
 	if (*s == '\0')
 		return (0);
-
-	c = 1 +  _strlen_recursion(s + 1);
-
-	return (c);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
