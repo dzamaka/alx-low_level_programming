d5733ae4e86b30d09d985f0c75a09da7a6dae9af
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-06 23:33:26 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - allocates memory using malloc and free
+ * @ptr: pointer to previous memory
+ * @old_size: old size in byte of memory
+ * @new_size: new size in byte
+ * Return: void pointer to new memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_location, *new_ptr;
+	unsigned int i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL) /* treat as normal malloc */
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_location = malloc(new_size);
+	new_ptr = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		new_location[i] = new_ptr[i];
+	free(ptr);
+	return (new_location);
+}
