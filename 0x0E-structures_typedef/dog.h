0eadea768a01aae275c4be92f971e659e5ab936f
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-10 15:43:44 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,7 +24,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog(char *name, float age, char *owner);
-void free_dog(struct dog *d);
 
 #endif
