54ab1c6d8bdd5104242b10f79e4605ea51b8f6e0
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-12 13:50:50 -0700

    Function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*func_p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	func_p = get_op_func(argv[2]);
+
+	if (func_p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", func_p(a, b));
+	return (0);
+}
