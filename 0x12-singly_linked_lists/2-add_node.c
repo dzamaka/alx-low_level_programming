ba4b3691ece756ec55afef51d024122ce25c91d0
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-20 14:19:14 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <string.h>
+
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
@@ -9,21 +11,27 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	size_t nchar;
+	
+	list_t *new_node;
+	int c;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
+	for (c = 0; dup_str[c] != '\0'; c++)
 		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = c;
+	new_node->next = *head;
 
-	new->len = nchar;
-	new->next = *head;
-	*head = new;
-
+	*head = new_node;
 	return (*head);
 }
