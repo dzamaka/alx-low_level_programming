a51a42e008e20ecf2f614a2e6014687a98324e8a
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-20 14:35:33 -0700

    Task 2 and 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,27 +11,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
-	list_t *new_node;
-	int c;
+	list_t *new;
+	size_t nchar;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
 		;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = *head;
 
-	*head = new_node;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
+
 	return (*head);
 }
