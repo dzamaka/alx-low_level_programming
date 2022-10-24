9b0ef089d961a4611cea67bdd6bdb31a2780180f
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-24 16:08:03 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all elements of a `listint_t` list
+ * @h: linked list head
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *new;
+	size_t count;
+
+	for (new = h, count = 0; new != NULL; count++, new = new->next)
+		printf("%d\n", new->n);
+
+	return (count);
+}
