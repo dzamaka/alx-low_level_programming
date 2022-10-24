e9fca2224d71bea950906c3b890f6b685728d673
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-24 16:34:02 -0700

    Task 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - print all elements of a `listint_t` list
+ * listint_len - find the number of elements in a linked `listint_t` list
  * @h: linked list head
- * Return: number of nodes
+ * Return: number of elements in linked list
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	const listint_t *c;
 	size_t count;
 
 	for (c = h, count = 0; c != NULL; count++, c = c->next)
-	printf("%d\n", c->n);
+		;
 
 	return (count);
 }
