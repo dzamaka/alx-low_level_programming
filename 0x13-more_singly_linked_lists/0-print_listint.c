91bad251a8784458eb4af649394d735af049c03e
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-25 14:52:12 -0700

    Task 1 update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,11 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *c;
 	size_t count;
 
-	for (c = h, count = 0; c != NULL; count++, c = c->next)
-	printf("%d\n", c->n);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
 	return (count);
 }
