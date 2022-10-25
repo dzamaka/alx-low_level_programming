d017f00c1a58d0fecba2513b2210aa4201c631eb
Amaka Anyanhun <amaka.anyanhun@gmail.com>
2022-10-24 17:12:41 -0700

    Task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * reverse_listint - Reverse a `listint_t` linked list
+ * @head: double pointer to head
+ * Description: Limited to going through loop only once.
+ * Not allowed to use `malloc` or `free` or arrays
+ * Can only declare a max of 2 variables.
+ * Return: pointer to first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *current;
+
+	if (*head == NULL)
+		return (NULL);
+
+	current = *head;
+	*head = current->next;
+	temp = (*head)->next;
+	current->next = NULL;
+	if (*head == NULL)
+	{
+		*head = current;
+		return (current);
+	}
+
+	while (temp != NULL)
+	{
+		(*head)->next = current;
+		current = *head;
+		*head = temp;
+		hold = (*head)->next;
+	}
+
+	(*head)->next = current;
+	return (*head);
+}
